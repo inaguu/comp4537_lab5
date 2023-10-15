95d8a695fcf2c014813766aacf82b784943dc3c9
inaguu <ianjguani@gmail.com>
2023-10-15 03:14:59 -0700

    final changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 let req_count = 0;
 
-const con = mysql.createConnection ({
+const con = mysql.createPool ({
     host: "sql.freedb.tech",
     user: "freedb_comp3920_iang",
     password: "suc2&%7*a%D4brU",
@@ -17,42 +17,75 @@ http.createServer((req, res) => {
     let q = url.parse(req.url, true) 
     let pathname = q.pathname
 
-    res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': '*'})
-
     if (pathname.includes("/lab5/api/v1/sql/") ) {
 
         let sql = pathname.substring(pathname.lastIndexOf('/') + 1)
         let clean_sql = sql.replace(/%20/g, " ")
 
         con.query(clean_sql, (err, result) => {
-            if (err) throw err
-            console.log(result)
-
-            for (let i = 0; i < result.length; i++) {
-                res.write(`<p>${result[i].user_id}, ${result[i].username}, ${result[i].email}</p><br>`)
+            if (err) {
+                res.writeHead(400, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                res.end("You got an SQL error, please check your SQL query")
             }
+            console.log(result)
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
             
+            let table = "<table>"
+            for (let i = 0; i < result.length; i++) {
+                table += `<tr><td>${result[i].patientid}</td><td>${result[i].name}</td><td>${result[i].dateOfBirth}</td></tr>`
+            }
+            table += "</table>"
+
+            res.write(table)
             res.end()
         })
-    } else if (req.method === "POST") {
+
+    } else if (req.method === "OPTIONS") {
+        res.writeHead(200, {
+            'access-control-allow-origin': '*',
+            'access-control-allow-methods': 'GET, POST, OPTIONS',
+            'access-control-allow-headers': 'Content-Type'
+        })
+        res.end()
+
+    } else if (req.method === "POST" && pathname == "/lab5/insert") {
         let body = ""
         
-        req.on("data", (chunk) => {
+        req.on('data', function(chunk) {
             if (chunk != null) {
                 body += chunk
             }
         })
 
-        console.log(body)
-
         req.on("end", () => {
-            res.end("We got your POST request")
+            let data = JSON.parse(body)
+            con.query(data.query, (err, result) => {
+                if (err) {
+                    res.writeHead(400, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                    res.end("You got an SQL error, please check your SQL query")
+                }
+                console.log(result)
+                res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                res.end("We got your POST request")
+            })
         })
 
-    }  else if (req.method === "GET") {
-        res.end("We got your GET request")
-    
+    }  else if (req.method === "GET" && pathname.includes("/lab5/select")) {
+        let sql = pathname.substring(pathname.lastIndexOf('/') + 1)
+        let clean_sql = sql.replace(/%20/g, " ")
+
+        con.query(clean_sql, (err, result) => {
+            if (err) {
+                res.writeHead(400, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+                res.end("You got an SQL error, please check your SQL query")
+            }
+            console.log(result)
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})            
+            res.end(JSON.stringify({response: `We got your GET request`, result}))
+        })
+        
     } else {
+        res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
         res.write("<p>home page</p>")
         res.end()
     }
