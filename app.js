a458e637f74b4c00fa3e7e0ed0c17821d0c4e0ac
inaguu <ianjguani@gmail.com>
2023-10-14 17:45:13 -0700

    added db and hosting

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const http = require('http')
 const url = require("url")
+const mysql = require("mysql")
 const port = 3000
 
 let req_count = 0;
+
+const con = mysql.createConnection ({
+    host: "sql.freedb.tech",
+    user: "freedb_comp3920_iang",
+    password: "suc2&%7*a%D4brU",
+    database: "freedb_comp3920NodeJS"
+})
+
 
 http.createServer((req, res) => {
     let q = url.parse(req.url, true) 
     let pathname = q.pathname
 
-    if (req.method === "POST" && pathname.includes("INSERT")) {
-        
+    if (pathname.includes("/lab5/api/v1/sql/") ) {
+
+        let sql = pathname.substring(pathname.lastIndexOf('/') + 1)
+        let clean_sql = sql.replace(/%20/g, " ")
+
+        con.query(clean_sql, (err, result) => {
+            if (err) throw err
+            console.log(result)
+            res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'})
+            res.end(result)
+        })
+    } 
+
+    if (req.method === "POST") {
+        console.log(req.body)
     }
 
-    if (req.method === "GET" && pathname.includes("SELECT")) {
+    if (req.method === "GET") {
         
     }
 
